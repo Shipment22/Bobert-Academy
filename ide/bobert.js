a376b0706d5923bb3df5d3dc89b796a305017e98
Shipment22 <crapple2@datazap.net>
2022-03-08 23:25:31 -0500

    working syntax highlighting

--- ide/bobert.js
+++ ide/bobert.js
@@ -1,23 +1,380 @@
 const editorDiv = document.querySelector('.editor')
-const scrollyscroll = document.querySelector('.editor .scroll-wraper')
 const output = document.getElementById('output')
 
-var thing = document.createElement("div");
-// thing.innerHTML = code;
-thing.style.whiteSpace = "pre";
-thing.style.fontFamily = "monospace";
-thing.style.fontSize = '12px'
-thing.style.lineHeight = '20px'
-thing.style.backgroundColor = "rgb(39, 40, 34)";
-thing.style.color = "white";
-thing.style.padding = "8px";
-thing.id = "myCode";
-thing.classList.add('editor-text')
-thing.contentEditable = true;
-thing.spellcheck = false;
-scrollyscroll.appendChild(thing);
+var codeDiv = document.createElement("div");
+codeDiv.style.whiteSpace = "pre";
+codeDiv.style.fontFamily = "monospace";
+codeDiv.style.fontSize = '12px'
+codeDiv.style.lineHeight = '20px'
+codeDiv.style.padding = "8px";
+codeDiv.classList.add('editor-text')
+codeDiv.contentEditable = true;
+codeDiv.spellcheck = false;
+document.querySelector('.editor .code-container').appendChild(codeDiv);
 
-const codeDiv = document.getElementById("myCode");
+var highlightDiv = document.createElement("div");
+highlightDiv.style.whiteSpace = "pre";
+highlightDiv.style.fontFamily = "monospace";
+highlightDiv.style.fontSize = '12px'
+highlightDiv.style.lineHeight = '20px'
+highlightDiv.style.padding = "8px"; 
+highlightDiv.classList.add('editor-highlight')
+document.querySelector('.editor .code-container').appendChild(highlightDiv);
+
+function highlight () {
+    var txt = codeDiv.innerHTML
+
+    let rep
+
+    switch(getFile(fileEditing).type) {
+        case 'html':
+
+
+            let valMatch = txt.match(/['"][^"']+['"]/gi)
+            for (o of valMatch) {
+                txt = txt.replace(o, `<span class="html_value">${o}</span>`)
+            }
+
+            let properties = [
+                'accept',
+                'accept-charset',
+                'accesskey',
+                'action',
+                'alt',
+                'async',
+                'autocomplete',
+                'autofocus',
+                'autoplay',
+                'charset',
+                'checked',
+                'cite',
+                'class',
+                'cols',
+                'colspan',
+                'content',
+                'contenteditable',
+                'controls',
+                'coords',
+                'data',
+                'data-',
+                'datetime',
+                'default',
+                'defer',
+                'dir',
+                'dirname',
+                'disabled',
+                'download',
+                'draggable',
+                'encrypte',
+                'for',
+                'form',
+                'formaction',
+                'headers',
+                'height',
+                'hidden',
+                'high',
+                'href',
+                'hreflang',
+                'http-equiv',
+                'id',
+                'ismap',
+                'kind',
+                'label',
+                'lang',
+                'list',
+                'loop',
+                'low',
+                'max',
+                'maxlength',
+                'media',
+                'method',
+                'min',
+                'multiple',
+                'muted',
+                'name',
+                'novalidate',
+                'onbort',
+                'onafterprint',
+                'onbeforeprint',
+                'onbeforeunload',
+                'onblur',
+                'oncanplay',
+                'oncanplaythrough',
+                'onchange',
+                'onclick',
+                'oncontextmenu',
+                'oncopy',
+                'oncuechange',
+                'oncut',
+                'ondblclick',
+                'ondrag',
+                'ondragend',
+                'ondragenter',
+                'ondragleave',
+                'ondragover',
+                'ondragstart',
+                'ondurationchange',
+                'onemptied',
+                'onended',
+                'onerror',
+                'onfocus',
+                'onhashchange',
+                'oninput',
+                'oninvalid',
+                'onkeydown',
+                'onkeypress',
+                'onkeyup',
+                'onload',
+                'onloadeddata',
+                'onloadedmetadata',
+                'onloadstart',
+                'onmousedown',
+                'onmousemove',
+                'onmouseout',
+                'onmouseover',
+                'onmouseup',
+                'onmousewheel',
+                'onoffline',
+                'ononline',
+                'onpagehide',
+                'onpageshow',
+                'onpaste',
+                'onpause',
+                'onplay',
+                'onplaying',
+                'onpopstate',
+                'onprogress',
+                'onratechange',
+                'onreset',
+                'onresize',
+                'onscroll',
+                'onsearch',
+                'onseeked',
+                'onseeking',
+                'onselect',
+                'onstalled',
+                'onstorage',
+                'onsubmit',
+                'onsuspend',
+                'ontimeupdate',
+                'ontoggle',
+                'onunload',
+                'onvolumechange',
+                'onwaiting',
+                'onwheel',
+                'open',
+                'optimum',
+                'pattern',
+                'placeholder',
+                'poster',
+                'preload',
+                'readonly',
+                'rel',
+                'require',
+                'reversed',
+                'rows',
+                'rowspan',
+                'sandbox',
+                'scope',
+                'selected',
+                'shape',
+                'size',
+                'sizes',
+                'span',
+                'spellcheck',
+                'src',
+                'srcdoc',
+                'srclang',
+                'srcset',
+                'start',
+                'start',
+                'step',
+                'style',
+                'tabindex',
+                'target',
+                'title',
+                'translate',
+                'type',
+                'value',
+                'width',
+                'warp'
+            ]
+
+            // for (o of properties) {
+            //     txt = txt.replace(new RegExp(`/span> ${o}=`, 'gi'), `/span> <span class="html_property">${o}</span>=`)
+            // }
+
+            for (o of properties) {
+                let oMatches = txt.match(RegExp(`${o}=<span class="html_value">`, 'gi'))
+                if (oMatches === null) continue
+                for (q of oMatches) {
+                    q = q.replace('<span class="html_value">', '')
+                    txt = txt.replace(q, `<span class="html_property">${q}</span><span class="html_value">`)
+                }
+                // txt = txt.replace(new RegExp(`&lt;${o} `, 'gi'), `<span class="html_valid-tag">&lt;${o}</span> `)
+                // txt = txt.replace(new RegExp(`&lt;${o}&gt;`, 'gi'), `<span class="html_valid-tag">&lt;${o}</span>&gt;`)
+                // txt = txt.replace(new RegExp(`/${o}&gt;`, 'gi'), `<span class="html_valid-tag">/${o}</span>&gt;`)
+            }
+
+            let validTags = [
+                'a',
+                'abbr',
+                'address',
+                'area',
+                'artacle',
+                'aside',
+                'audio',
+                'b',
+                'base',
+                'bdi',
+                'bdo',
+                'blockquote',
+                'body',
+                'br',
+                'button',
+                'canvas',
+                'cite',
+                'code',
+                'col',
+                'colgroup',
+                'data',
+                'datalist',
+                'dd',
+                'del',
+                'details',
+                'dfn',
+                'dialog',
+                'div',
+                'div',
+                'dl',
+                'dt',
+                'em',
+                'embed',
+                'feildset',
+                'figcaption',
+                'figure',
+                'footer',
+                'form',
+                'h1',
+                'h2',
+                'h3',
+                'h4',
+                'h5',
+                'h5',
+                'h6',
+                'head',
+                'header',
+                'hr',
+                'html',
+                'i',
+                'iframe',
+                'img',
+                'input',
+                'ins',
+                'kbd',
+                'label',
+                'legend',
+                'li',
+                'link',
+                'main',
+                'map',
+                'mark',
+                'meta',
+                'nav',
+                'noscript',
+                'object',
+                'ol',
+                'optgroup',
+                'option',
+                'output',
+                'p',
+                'param',
+                'picture',
+                'pre',
+                'progress',
+                'q',
+                'rp',
+                'rt',
+                'ruby',
+                's',
+                'samp',
+                'script',
+                'select',
+                'small',
+                'source',
+                'span',
+                'strong',
+                'style',
+                'sub',
+                'summary',
+                'sup',
+                'svg',
+                'table',
+                'tbody',
+                'td',
+                'template',
+                'textarea',
+                'tfoot',
+                'th',
+                'thead',
+                'time',
+                'title',
+                'tr',
+                'track',
+                'u',
+                'ul',
+                'var',
+                'video',
+                'wbr'
+            ]
+
+            for (o of validTags) {
+                let oMatches = txt.match(RegExp(`(&lt;|/)${o}(\\s|&gt;)`, 'gi'))
+                if (oMatches === null) continue
+                for (q of oMatches) {
+                    txt = txt.replace(q, `<span class="html_valid-tag">${q}</span>`)
+                }
+                // txt = txt.replace(new RegExp(`&lt;${o} `, 'gi'), `<span class="html_valid-tag">&lt;${o}</span> `)
+                // txt = txt.replace(new RegExp(`&lt;${o}&gt;`, 'gi'), `<span class="html_valid-tag">&lt;${o}</span>&gt;`)
+                // txt = txt.replace(new RegExp(`/${o}&gt;`, 'gi'), `<span class="html_valid-tag">/${o}</span>&gt;`)
+            }
+
+
+            rep = [
+                [
+                    /&lt;/g,
+                    '<span class="html_angle-bracket"><</span>'
+                ],
+                [
+                    /&gt;/g,
+                    '<span class="html_angle-bracket">></span>'
+                ],
+                [
+                    new RegExp('<span class="html_angle-bracket"><</span>!--', 'g'),
+                    '<span class="html_comment">&lt;!--'
+                ],
+                [
+                    new RegExp('--<span class="html_angle-bracket">></span>', 'g'),
+                    '--&gt;</span>'
+                ],
+                [
+                    /!DOCTYPE html/g,
+                    '<span class="html_doctype">!DOCTYPE</span> <span class="html_html-doctype">html</span>'
+                ],
+            ]
+
+            break
+        default:
+            rep = []
+            break
+    }
+
+    for (i in rep) {
+        txt = txt.replace(rep[i][0], rep[i][1])
+    }
+
+    highlightDiv.innerHTML = txt
+}
 
 function editorLineNums() {
     let lineNums = document.querySelector('.editor-line-nums')
@@ -76,7 +433,7 @@ function addFile(name, type, content) {
 </head>
 <body>
     Bobert
-
+    <!-- bobert -->
     <script src="main.js"></script>
 </body>
 </html>`
@@ -131,6 +488,7 @@ function setFile(name) {
     codeDiv.textContent = file.content
     fileEditing = file.name
     editorLineNums()
+    highlight()
 }
 
 function runFile(name) {
@@ -170,6 +528,13 @@ function grabFileFromEditor() {
 
 addFile('index.html')
 setFile('index.html')
+
+var startMS = Date.now();
+highlight(codeDiv);
+var endMS = Date.now();
+document.getElementById("time").innerHTML = "It took " + (endMS - startMS) + " ms to highlight the code.";
+
+
 addFile('style.css')
 addFile('main.js')
 
@@ -178,19 +543,19 @@ document.getElementById('new-file').onclick = () => {
     if (name) addFile(name)
 }
 
-let updateTimer = 0
+setInterval(highlight, 1000)
 
-setInterval(() => {
-    updateTimer ++
-}, 10)
+codeDiv.onkeydown = codeDiv.onkeyup = function (e) {
 
-codeDiv.onkeydown = e => {
-    setTimeout(() => {
-        grabFileFromEditor()
-        if (updateTimer > 5) {
-            runFile('index.html')
-            updateTimer = 0
-        }
-    }, 20)
+    const startMS = Date.now();
+    highlight(codeDiv)
+    const endMS = Date.now();
+    
+    document.getElementById("time").innerHTML = "It took " + (endMS - startMS) + " ms to highlight the code.";
+
     editorLineNums()
+    grabFileFromEditor()
+    runFile('index.html')
+
+    highlighting = false
 }
